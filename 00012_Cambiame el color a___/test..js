be01db108f50610893dfba5e4c0704fea3a0c955
fedescarpa+mumuki@gmail.com <fedescarpa+mumuki@gmail.com>
2018-11-26 15:46:07 -0500

    Mumuki Export on 2018-11-26 15:46:07 -0500

--- 00012_Cambiame el color a___/test..js
+++ 00012_Cambiame el color a___/test..js
@@ -1,6 +1,7 @@
 /*<output#*//*#output>*/
 /*<tests#*/it("Cambio style color", function() {
-  should(document.querySelector("body").style.color).eql("pink")
+  true.should.be.eql(false, 'HOLA' + JSON.stringify(document.querySelector("body")))
+  document.querySelector("body").style.color.should.eql("pink")
 });/*#tests>*/
 /*<options#*/output_ignore_scripts: true
 output_ignore_styles: true/*#options>*/
