bfddec2ba922a108888be4f0ff320c7ed74d48a9
fedescarpa+mumuki@gmail.com <fedescarpa+mumuki@gmail.com>
2018-11-26 15:31:50 -0500

    Mumuki Export on 2018-11-26 15:31:50 -0500

--- 00012_Cambiame el color a___/test..js
+++ 00012_Cambiame el color a___/test..js
@@ -1,12 +1,12 @@
 /*<output#*//*#output>*/
 /*<tests#*/
-function foo(document) {
-  return document.querySelector("body").style.backgroundColor == "pink";
+function foo() {
+  return this.querySelector("body").style.backgroundColor == "pink";
 }
 
 it("Cambio de color", function() {
   _dispatch_('load', document);
-  _wait_for_(() => foo(document), function() {
+  _wait_for_(foo.bind(document), function() {
       document.querySelector("body").style.backgroundColor.should.eql("pink");
   })
 });/*#tests>*/
