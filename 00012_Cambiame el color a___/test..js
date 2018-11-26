9a97aebf1dffbde7666090c25c542360364b27df
fedescarpa+mumuki@gmail.com <fedescarpa+mumuki@gmail.com>
2018-11-26 15:52:16 -0500

    Mumuki Export on 2018-11-26 15:52:16 -0500

--- 00012_Cambiame el color a___/test..js
+++ 00012_Cambiame el color a___/test..js
@@ -1,4 +1,11 @@
-/*<output#*//*#output>*/
+/*<output#*/<html>
+  <head>
+    <script src="script.js"></script>
+  </head>
+  <body style="color: pink;">
+    <marquee style="font-size:32px;font-family:Arial">Alto Fondo!</marquee>
+  </body>
+</html>/*#output>*/
 /*<tests#*/it("Cambio style color", function() {
   true.should.be.eql(false, 'HOLA' + document.querySelector("body").getAttribute('style'))
   document.querySelector("body").style.color.should.eql("pink")
