73bf83e1ffb0b7521dc6cc2f82bc9d67e618053b
lucho@mumuki.org <lucho@mumuki.org>
2018-11-26 13:40:51 -0500

    Mumuki Export on 2018-11-26 13:40:51 -0500

---
+++ 00012_Test/default.js
@@ -0,0 +1,2 @@
+/*<file_1.html#*//*#file_1.html>*/
+/*<script.js#*//*#script.js>*/
--- 00012_Test/test..js
+++ 00012_Test/test..js
@@ -3,10 +3,9 @@
 </html>/*#output>*/
 /*<tests#*/it("Definir una func que devuelva 2", function() {
   _confirm_response_ = true;
-  _dispatch_('load')
+  _dispatch_('load');
 
-  
-  _last_alert_message_.should.eql("hola", "<b><u>La funcion debería retornar 2'</u></b>")
+  _last_alert_message_.should.eql("hola", "<b><u>La funcion debería retornar 2'</u></b>");
 });/*#tests>*/
 /*<options#*/output_ignore_scripts: true
 output_ignore_styles: true/*#options>*/
