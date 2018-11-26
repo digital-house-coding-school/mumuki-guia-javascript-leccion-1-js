63785a2357422bbe922ab64f5dd7dde9989cdff5
fedescarpa+mumuki@gmail.com <fedescarpa+mumuki@gmail.com>
2018-11-26 15:52:34 -0500

    Mumuki Export on 2018-11-26 15:52:34 -0500

--- 00012_Cambiame el color a___/test..js
+++ 00012_Cambiame el color a___/test..js
@@ -6,9 +6,9 @@
     <marquee style="font-size:32px;font-family:Arial">Alto Fondo!</marquee>
   </body>
 </html>/*#output>*/
-/*<tests#*/it("Cambio style color", function() {
-  true.should.be.eql(false, 'HOLA' + document.querySelector("body").getAttribute('style'))
-  document.querySelector("body").style.color.should.eql("pink")
-});/*#tests>*/
+/*<tests#*/// it("Cambio style color", function() {
+//  true.should.be.eql(false, 'HOLA' + document//.querySelector("body").getAttribute('style'))
+  //document.querySelector("body").style.color.should.eql("pink")
+//});/*#tests>*/
 /*<options#*/output_ignore_scripts: true
 output_ignore_styles: true/*#options>*/
